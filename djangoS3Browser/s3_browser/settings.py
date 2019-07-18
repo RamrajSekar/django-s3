fadb52874bda71571ef4bb7d79c89b622112a890
Ramraj Sekar <sekar.ramraj@gmail.com>
2019-07-18 16:54:51 +0530

    Update settings.py
    test

--- djangoS3Browser/s3_browser/settings.py
+++ djangoS3Browser/s3_browser/settings.py
@@ -1,12 +1,12 @@
-AWS_ACCESS_KEY_ID = "ae0-reliability-appengineplatform-u3"
-AWS_SECRET_ACCESS_KEY = "l9CXod2h+cIBCdaacZn9Plaxa9r68GXOy2Z3wrwH"
-AWS_STORAGE_BUCKET_NAME = "ae0-reliability-appengineplatform-qa-1"
-AWS_S3_ENDPOINT_URL = "https://ecsams.bnymellon.net:9021"
+AWS_ACCESS_KEY_ID = "TEST"
+AWS_SECRET_ACCESS_KEY = "TEST"
+AWS_STORAGE_BUCKET_NAME = "TEST"
+AWS_S3_ENDPOINT_URL = "https://TEST1"
 AWS_AUTO_CREATE_BUCKET = True
 AWS_QUERYSTRING_AUTH = False
 S3_BROWSER_SETTINGS = "djangoS3Browser"
 TEMPLATES = [{ 'OPTIONS': {'libraries': {'s3-load': 'djangoS3Browser.templatetags.s3-tags',},}}]
-SECRET_KEY = 'y130-j9oz4r5aoamn_n=+s-*7n)*3^s$jmf4(qw6ik28()g^(n'
+SECRET_KEY = 'TEST'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 ROOT_URLCONF= 'djangoS3Browser.s3_browser.urls'
